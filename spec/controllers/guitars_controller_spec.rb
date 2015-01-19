d91b61cff4abb359d034eae674406b022628cfde
David Fisher <tibbon@gmail.com>
2015-01-19 16:38:51 -0500

    Added some comments to the specs

--- spec/controllers/guitars_controller_spec.rb
+++ spec/controllers/guitars_controller_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.describe GuitarsController do
   describe "GET index" do
+
+    # In config/routes.rb
+    # You'll need to add something to make this work
     it "routes /guitars to guitar#index" do
       expect(get: '/guitars').to route_to(controller: 'guitars', action: 'index')
     end
 
+    # You'll edit controller/guitars_controller.rb
+    # to make this one pass
     it 'assigns @guitars to be all guitars from the database' do
       guitar_one = Guitar.create
       guitar_two = Guitar.create
@@ -14,6 +19,8 @@ RSpec.describe GuitarsController do
       expect(assigns(:guitars)).to eq([guitar_one, guitar_two, guitar_three])
     end
 
+    # You shouldn't have to do *anything* to make this spec pass if all
+    # of the others are passing
     it 'renders the index template' do
       get :index
       expect(response).to render_template('index')
