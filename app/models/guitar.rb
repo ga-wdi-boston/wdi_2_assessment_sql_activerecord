499532a3d85f3aad4393991ef10553e2746463f0
Doug <djmacdonald@uchicago.edu>
2015-01-26 09:35:02 -0500

    My SQL line  is not working.

--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -6,5 +6,10 @@ class Guitar < ActiveRecord::Base
     # You are looking for a string similar to:
     # SELECT * FROM posts
 
+    find_by_sql(
+      "SELECT *
+      FROM Guitars
+      WHERE Make = 'Fender'"
+      )
   end
 end
