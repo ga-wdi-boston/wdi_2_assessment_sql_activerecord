33360243c483acb4fb28edcab86a0d8337d6827f
lady3bean <annehawleybrett@gmail.com>
2015-01-26 09:36:26 -0500

    getting weird error I can't debug - rake db:migrate:status rake aborted! Errno::ENOENT: No such file or directory @ dir_initialize - /Users/hawley/wdi/week2/wdi_2_assessment_sql_activerecord/db/migrate /Users/hawley/.rvm/gems/ruby-2.2.0/gems/activerecord-4.2.0/lib/active_record/railties/databases.rake:101:in `open' /Users/hawley/.rvm/gems/ruby-2.2.0/gems/activerecord-4.2.0/lib/active_record/railties/databases.rake:101:in `foreach' /Users/hawley/.rvm/gems/ruby-2.2.0/gems/activerecord-4.2.0/lib/active_record/railties/databases.rake:101:in `each' /Users/hawley/.rvm/gems/ruby-2.2.0/gems/activerecord-4.2.0/lib/active_record/railties/databases.rake:101:in `grep' /Users/hawley/.rvm/gems/ruby-2.2.0/gems/activerecord-4.2.0/lib/active_record/railties/databases.rake:101:in `block (4 levels) in <top (required)>' /Users/hawley/.rvm/gems/ruby-2.2.0/gems/activerecord-4.2.0/lib/active_record/railties/databases.rake:99:in `each' /Users/hawley/.rvm/gems/ruby-2.2.0/gems/activerecord-4.2.0/lib/active_record/railties/databases.rake:99:in `flat_map' /Users/hawley/.rvm/gems/ruby-2.2.0/gems/activerecord-4.2.0/lib/active_record/railties/databases.rake:99:in `block (3 levels) in <top (required)>' Tasks: TOP => db:migrate:status (See full trace by running task with --trace)

--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -1,5 +1,7 @@
 class Guitar < ActiveRecord::Base
   def self.fender_guitars
+
+    
     # This method should return a raw string of a SQL command to return
     # all guitars with the make of 'Fender'
     # Do not run an ActiveRecord method in here!
