f3e77e03080b2741fd10473ae00450ff8a780b51
J-Weeks <weeksjasons@gmail.com>
2015-01-26 10:04:19 -0500

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150126145435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "guitars", force: :cascade do |t|
+    t.string  "make"
+    t.string  "model"
+    t.decimal "price"
+  end
 
 end
