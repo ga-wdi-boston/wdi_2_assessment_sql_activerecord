d8b3c2acac7d16edb84c4eaf8c700459bf531ea2
Ashlinn Brennan <asbren13@gmail.com>
2015-01-26 09:22:18 -0500

    assessment complete

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -1,2 +1,14 @@
 class GuitarsController < ApplicationController
+  def index
+    @guitars = Guitar.all
+  end
+
+  def show
+    @guitar = Guitar.find(params[:id])
+  end
+
+  def create
+    @guitar = Guitar.create
+  end
+
 end
